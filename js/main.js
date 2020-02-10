c500eb3c9e75bfc3748d9d1f4e009cdbcf7ecb8c
Alexander Lodygin <lodigin.alexander@gmail.com>
2020-02-10 19:00:55 +0200

    image compression

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //colcade.js=====================
-var colc = new Colcade('.grid', {
-  columns: '.grid-col',
-  items: '.grid-item'
-});
+document.addEventListener('DOMContentLoaded', function(){
+  var colc = new Colcade('.grid', {
+    columns: '.grid-col',
+    items: '.grid-item'
+  });
+}, false);
