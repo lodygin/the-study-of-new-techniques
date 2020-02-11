8d0c0d3c711808c26da6ee1ebb1d033b376600ee
Alexander Lodygin <lodigin.alexander@gmail.com>
2020-02-11 20:23:14 +0200

    Block Intro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.intro__list');
+  const navLinks = document.querySelectorAll('.intro__item');
+
+  burger.addEventListener('click', () => {
+    //Toggle Nav
+    nav.classList.toggle('list-active');
+    burger.classList.toggle('burger-active');
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+      }
+    });
+  });
+}
+
+navSlide();
+
 //colcade.js=====================
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
   var colc = new Colcade('.grid', {
     columns: '.grid-col',
     items: '.grid-item'
